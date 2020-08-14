fc59828d22e6ac8b05eae9c140efa347861e298f
hakolao <okkohakola@gmail.com>
2020-08-14 20:36:32 +0300

    More debug info

--- src/vulkan_debug.c
+++ src/vulkan_debug.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/07 17:35:54 by ohakola           #+#    #+#             */
-/*   Updated: 2020/08/10 14:00:25 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/08/14 20:18:07 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL	debug_callback(
 	void *p_user_data) {
 	(void)message_type;
 	(void)p_user_data;
-	if (message_severity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
-		ft_dprintf(2, "Validation layer: %s\n", p_callback_data->pMessage);
+	if (message_severity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT)
+		ft_dprintf(2, "Validation layer:\n%s\n", p_callback_data->pMessage);
 	return (VK_FALSE);
 }
 
@@ -68,6 +68,7 @@ void									vulkan_populate_debug_messenger_create_info(
 	create_info->sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
 	create_info->messageSeverity =
 		VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
 		VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
 		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
 	create_info->messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |

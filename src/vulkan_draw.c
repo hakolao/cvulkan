cdcf5cf304ba0dc470998ae59d8741f384424be8
hakolao <okkohakola@gmail.com>
2020-08-15 21:26:15 +0300

    Extract image present function

--- src/vulkan_draw.c
+++ src/vulkan_draw.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 15:33:11 by ohakola           #+#    #+#             */
-/*   Updated: 2020/08/15 20:55:42 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/08/15 21:15:29 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,22 +39,37 @@ static void		vulkan_update_uniform_buffer(t_cvulkan *app,
 	ft_memcpy(data, &ubo, sizeof(ubo));
 	vkUnmapMemory(app->vk_logical_device,
 		app->vk_uniform_buffers_memory[currentImage]);
-
-
 }
 
-void			vulkan_draw(t_cvulkan *app)
+static void		submit_draw_queue(t_cvulkan *app, uint32_t imageIndex,
+				VkSubmitInfo *submitInfo)
 {
-	uint32_t			imageIndex;
-	VkResult			res;
-	VkSubmitInfo		submitInfo;
-	VkPresentInfoKHR	presentInfo;
+	ft_memset(submitInfo, 0, sizeof(*submitInfo));
+	submitInfo->sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+	submitInfo->waitSemaphoreCount = 1;
+	submitInfo->pWaitSemaphores = (VkSemaphore[1]){
+		app->vk_img_available_semaphores[app->vk_current_frame]};
+	submitInfo->pWaitDstStageMask = (VkPipelineStageFlags[1]){
+		VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+	submitInfo->commandBufferCount = 1;
+	submitInfo->pCommandBuffers = &app->vk_command_buffers[imageIndex];
+	submitInfo->signalSemaphoreCount = 1;
+	submitInfo->pSignalSemaphores = (VkSemaphore[1]){
+		app->vk_rndr_finished_semaphores[app->vk_current_frame]};
+	vkResetFences(app->vk_logical_device, 1,
+		&app->vk_in_flight_fences[app->vk_current_frame]);
+	error_check(vkQueueSubmit(app->vk_graphics_queue, 1, submitInfo,
+		app->vk_in_flight_fences[app->vk_current_frame]) != VK_SUCCESS,
+			"Failed to submit draw command buffer!");
+}
 
-	vkWaitForFences(app->vk_logical_device, 1,
-		&app->vk_in_flight_fences[app->vk_current_frame], VK_TRUE, UINT64_MAX);
+static void		acquire_swap_chain_image(t_cvulkan *app, uint32_t *imageIndex)
+{
+	VkResult			res;
+
 	res = vkAcquireNextImageKHR(app->vk_logical_device, app->vk_swap_chain,
 		UINT64_MAX, app->vk_img_available_semaphores[app->vk_current_frame],
-		VK_NULL_HANDLE, &imageIndex);
+		VK_NULL_HANDLE, imageIndex);
 	if (res == VK_ERROR_OUT_OF_DATE_KHR)
 	{
 		vulkan_recreate_swapchain(app);
@@ -63,33 +78,18 @@ void			vulkan_draw(t_cvulkan *app)
 	else
 		error_check(res != VK_SUCCESS && res != VK_SUBOPTIMAL_KHR,
 			"Failed to acquire swap chain image!");
-	if (app->vk_images_in_flight[imageIndex] != VK_NULL_HANDLE)
-		vkWaitForFences(app->vk_logical_device, 1,
-			&app->vk_images_in_flight[imageIndex], VK_TRUE, UINT64_MAX);
-	app->vk_images_in_flight[imageIndex] =
-		app->vk_in_flight_fences[app->vk_current_frame];
-	vulkan_update_uniform_buffer(app, imageIndex);
-	ft_memset(&submitInfo, 0, sizeof(submitInfo));
-	submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-	submitInfo.waitSemaphoreCount = 1;
-	submitInfo.pWaitSemaphores = (VkSemaphore[1]){
-		app->vk_img_available_semaphores[app->vk_current_frame]};
-	submitInfo.pWaitDstStageMask = (VkPipelineStageFlags[1]){
-		VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
-	submitInfo.commandBufferCount = 1;
-	submitInfo.pCommandBuffers = &app->vk_command_buffers[imageIndex];
-	submitInfo.signalSemaphoreCount = 1;
-	submitInfo.pSignalSemaphores = (VkSemaphore[1]){
-		app->vk_rndr_finished_semaphores[app->vk_current_frame]};
-	vkResetFences(app->vk_logical_device, 1,
-		&app->vk_in_flight_fences[app->vk_current_frame]);
-	error_check(vkQueueSubmit(app->vk_graphics_queue, 1, &submitInfo,
-		app->vk_in_flight_fences[app->vk_current_frame]) != VK_SUCCESS,
-			"Failed to submit draw command buffer!");
+}
+
+static void		present_image(t_cvulkan *app, uint32_t imageIndex)
+{
+	VkPresentInfoKHR	presentInfo;
+	VkResult			res;
+
 	ft_memset(&presentInfo, 0, sizeof(presentInfo));
 	presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
 	presentInfo.waitSemaphoreCount = 1;
-	presentInfo.pWaitSemaphores = submitInfo.pSignalSemaphores;
+	presentInfo.pWaitSemaphores = (VkSemaphore[1]){
+		app->vk_rndr_finished_semaphores[app->vk_current_frame]};
 	presentInfo.swapchainCount = 1;
 	presentInfo.pSwapchains = (VkSwapchainKHR[1]){app->vk_swap_chain};
 	presentInfo.pImageIndices = &imageIndex;
@@ -99,5 +99,23 @@ void			vulkan_draw(t_cvulkan *app)
 		vulkan_recreate_swapchain(app);
 	else
 		error_check(res != VK_SUCCESS, "Failed to present swap chain image!");
+}
+
+void			vulkan_draw(t_cvulkan *app)
+{
+	uint32_t			imageIndex;
+	VkSubmitInfo		submitInfo;
+
+	vkWaitForFences(app->vk_logical_device, 1,
+		&app->vk_in_flight_fences[app->vk_current_frame], VK_TRUE, UINT64_MAX);
+	acquire_swap_chain_image(app, &imageIndex);
+	if (app->vk_images_in_flight[imageIndex] != VK_NULL_HANDLE)
+		vkWaitForFences(app->vk_logical_device, 1,
+			&app->vk_images_in_flight[imageIndex], VK_TRUE, UINT64_MAX);
+	app->vk_images_in_flight[imageIndex] =
+		app->vk_in_flight_fences[app->vk_current_frame];
+	vulkan_update_uniform_buffer(app, imageIndex);
+	submit_draw_queue(app, imageIndex, &submitInfo);
+	present_image(app, imageIndex);
 	app->vk_current_frame = (app->vk_current_frame + 1) % MAXFRAMESINFLIGHT;
 }
